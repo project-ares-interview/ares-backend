741a552a5a1feda95f63b08a79fe86d7cc13567e
HanByeol Jang <j.1star.0726@gmail.com>
2025-09-03 17:31:05 +0900

    feat: 소셜 로그인, 회원가입에 대하여 seriailzer, view, adapter 작성

--- ares/api/serializers/v1/user.py
+++ ares/api/serializers/v1/user.py
@@ -18,6 +18,28 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         return user
 
 
+class SocialUserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(required=False, write_only=True, allow_null=True, allow_blank=True)
+
+    class Meta:
+        model = User
+        fields = ("email", "password", "name", "gender", "birth", "phone_number")
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "email": {"read_only": True},
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+        user.save()
+        return user
+
+
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
---
+++ ares/api/views/v1/social.py
@@ -0,0 +1,71 @@
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
+from django.core.signing import BadSignature, Signer
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from ares.api.adapter import NewSocialUserException
+from ares.api.serializers.v1.user import SocialUserRegisterSerializer
+from ares.settings import CLIENT_HOST, CLIENT_PORT
+
+
+class CustomSocialLoginView(SocialLoginView):
+    def post(self, request, *args, **kwargs):
+        self.request = request
+        self.serializer = self.get_serializer(data=self.request.data)
+
+        try:
+            self.serializer.is_valid(raise_exception=True)
+        except NewSocialUserException as e:
+            sociallogin = e.sociallogin
+            # New user: return signed social data for registration
+            signer = Signer()
+            signed_data = signer.sign_object({
+                "email": sociallogin.user.email,
+                "name": sociallogin.user.name or "",
+            })
+            response_data = {
+                "status": "registration_required",
+                "signed_data": signed_data,
+                "email": sociallogin.user.email,
+                "name": sociallogin.user.name or "",
+            }
+            return Response(response_data, status=status.HTTP_200_OK)
+
+        # Existing user: log them in
+        return super(SocialLoginView, self).post(request, *args, **kwargs)
+
+
+class GoogleLogin(CustomSocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = f"http://{CLIENT_HOST}:{CLIENT_PORT}"
+    client_class = OAuth2Client
+
+
+class GoogleRegisterView(CreateAPIView):
+    serializer_class = SocialUserRegisterSerializer
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        signer = Signer()
+        signed_data = self.request.data.get("signed_data")
+        
+        if not signed_data:
+            # This should ideally be caught by a custom validation in the serializer
+            return
+            
+        try:
+            social_data = signer.unsign_object(signed_data)
+        except BadSignature:
+            # This should also be caught by validation
+            return
+
+        # Pass the validated social data to the save method
+        # This will override the read_only fields
+        serializer.save(
+            email=social_data.get("email"),
+            name=self.request.data.get("name")
+        )
