4118365d2ce0a7daa4bc10e0a277df715d8fc98e
HanByeol Jang <j.1star.0726@gmail.com>
2025-09-03 17:33:51 +0900

    feat: admin, apps, urls 설정

--- ares/api/admin.py
+++ ares/api/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from ares.api.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("email", "name", "is_staff")
+    search_fields = ("email", "name")
+    ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Personal info",
+            {"fields": ("name", "gender", "birth", "phone_number")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password", "password2"),
+            },
+        ),
+    )
