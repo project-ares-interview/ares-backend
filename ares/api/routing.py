d582afd74a462e327cab4c03121ebc91dba24f79
Jang-Eunho <dmsgh619@gmail.com>
2025-09-12 17:20:02 +0900

    WIP

---
+++ ares/api/routing.py
@@ -0,0 +1,8 @@
+# ares/api/routing.py
+from django.urls import re_path
+
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r"ws/interview/coach/$", consumers.InterviewConsumer.as_asgi()),
+]
