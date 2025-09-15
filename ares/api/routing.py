0bc8d5f34ae001da2ce55918a447222f53753b81
HanByeol Jang <j.1star.0726@gmail.com>
2025-09-15 18:46:37 +0900

    WIP

--- ares/api/routing.py
+++ ares/api/routing.py
@@ -1,8 +1,9 @@
 # ares/api/routing.py
 from django.urls import re_path
-
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/interview/coach/$", consumers.InterviewConsumer.as_asgi()),
+    re_path(r'ws/interview/results/(?P<sid>[\w-]+)/$', consumers.InterviewResultsConsumer.as_asgi()),
+    re_path(r'ws/interview/audio/(?P<sid>[\w-]+)/$', consumers.InterviewAudioConsumer.as_asgi()),
+    re_path(r'ws/interview/video/(?P<sid>[\w-]+)/$', consumers.InterviewVideoConsumer.as_asgi()),
 ]
